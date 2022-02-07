3c6f612f4d0ad3b83f2382250628add7970c1de2
muhamadfikry <muhmdfikry25@gmail.com>
2022-02-07 14:15:11 +0700

    muhmdfikry

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,83 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard', [
+        "title" => "dashboard"
+    ]);
+});
+
+Route::get('/admin', function () {
+    return view('admin', [
+    ]);
+});
+
+Route::get('/project', function () {
+    return view('project', [
+        "title" => "project"
+    ]);
+});
+Route::get('/analis', function () {
+    return view('analis', [
+        "title" => "analis"
+    ]);
+});
+
+Route::get('/desain', function () {
+    return view('desain', [
+    ]);
+});
+
+Route::get('/developer', function () {
+    return view('developer', [
+        "title" => "developer"
+    ]);
+});
+
+Route::get('/marketing', function () {
+    return view('marketing', [
+        "title" => "marketing"
+    ]);
+});
+
+Route::get('/proyek', function () {
+    return view('proyek', [
+    ]);
+});
+
+Route::get('/mom', function () {
+    return view('mom', [
+        "title" => "mom"
+    ]);
+});
+
+Route::get('/pendokumen', function () {
+    return view('pendokumen', [
+        "title" => "pendokumen"
+    ]);
+});
+
+Route::get('/dokproject', function () {
+    return view('dokproject', [
+    ]);
+});
+
+Route::get('/meeting', function () {
+    return view('meeting', [
+        "title" => "meeting"
+    ]);
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('users', \App\Http\Controllers\UserController::class)
+    ->middleware('auth');
+
+Route::resource('post', PostController::class);
